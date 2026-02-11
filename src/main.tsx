9d5eedd84c7d32934dca58efcc6684de76ba9bee
kattprinsen <1647312+Kattprinsen@users.noreply.github.com>
2026-02-11 16:11:32 +0100

    React Routing implemented

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import { ToolsPage } from './pages/ToolsPage/ToolsPage.tsx'
+import { ErrorPage } from './pages/ErrorPage/ErrorPage.tsx'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/tools" element={<ToolsPage />} />
+      </Routes>
+    </BrowserRouter>,
+  </StrictMode>
 )
---
+++ src/pages/ToolsPage/ToolsPage.tsx
@@ -0,0 +1,9 @@
+import { Navbar } from "../../components/layout"
+
+export const ToolsPage = () => {
+    return (
+    <div>
+        <Navbar />
+        ToolsPage
+    </div>)
+}
